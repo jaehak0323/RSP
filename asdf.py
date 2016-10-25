44c7ff2e50cfdb5fcca722b8a32069e7948c8ca4
Jaehak <jahak0323@naver.com>
2016-10-25 18:53:16 +0900

    first commit

---
+++ asdf.py
@@ -0,0 +1,66 @@
+import random
+def main():
+	win = 0
+	lose = 0 
+	draw = 0
+	counter = 0
+	while counter <10 :
+		computer = random.randint(1,3)
+		player = input()
+		if int(player) == 1:
+			if computer == 1:
+				draw += 1
+				counter += 1
+				print("비겻습니다")
+			if computer == 2:
+				lose += 1
+				counter += 1
+				print("졌습니다")
+			if computer ==3:
+				win += 1
+				counter += 1
+				print("이겼습니다")
+		if int(player) == 2:
+			if computer == 1:
+				win += 1
+				counter += 1
+				print("이겼습니다")
+			if computer == 2:
+				draw += 1
+				counter += 1
+				print("비겻습니다")
+			if computer ==3:
+				lose += 1
+				counter += 1
+				print("졌습니다")
+		if int(player) == 3 :
+			if computer == 1:
+				lose += 1
+				counter += 1
+				print("졌습니다")
+			if computer == 2:
+				win += 1
+				counter += 1
+				print("이겼습니다")
+			if computer == 3 :
+				draw += 1
+				counter += 1
+				print("비겻습니다")
+		if int(player)> 3 : 
+			print("ㅗ")
+		if int(player) < 1 :
+			print("ㅗ")
+	print(win)
+	print(lose)
+	print(draw)
+
+main()
+
+
+
+
+
+
+
+
+
